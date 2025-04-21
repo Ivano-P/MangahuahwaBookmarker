1506dcb671795c3cb4b099901dd026551c70f6f7
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2025-04-21 20:40:39 +0200

    Added boostrap and setup simple presentation page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,15 @@
 import { Routes } from '@angular/router';
-import {HeaderComponent} from "./components/header/header.component";
-import {FooterComponent} from "./components/footer/footer.component";
 import {LoginComponent} from "./components/login/login.component";
 import {BookmarkerComponent} from "./components/bookmarker/bookmarker.component";
 import {ContactComponent} from "./components/contact/contact.component";
 import {PresentationComponent} from "./components/presentation/presentation.component";
+import {DiscoverComponent} from "./components/discover/discover.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'presentation', pathMatch: 'full'},
-  //{path: 'header', component: HeaderComponent,},
-  //{path: 'footer', component: FooterComponent,},
   {path: 'presentation', component: PresentationComponent,},
   {path: 'contact', component: ContactComponent,},
   {path: 'bookmarker', component: BookmarkerComponent,},
-  {path: 'login', component: LoginComponent,}
+  {path: 'login', component: LoginComponent,},
+  {path: 'discover', component: DiscoverComponent,},
 ];
--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-presentation',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './presentation.component.html',
   styleUrl: './presentation.component.css'
 })
