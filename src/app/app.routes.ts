6ce783b43a9ac1bc863acce8d7ca4d937b407952
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2025-04-21 18:24:35 +0200

    routes set up
    set up routes for components (login, presentation, contact and bookmarker)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {FooterComponent} from "./components/footer/footer.component";
+import {LoginComponent} from "./components/login/login.component";
+import {BookmarkerComponent} from "./components/bookmarker/bookmarker.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {PresentationComponent} from "./components/presentation/presentation.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'presentation', pathMatch: 'full'},
+  //{path: 'header', component: HeaderComponent,},
+  //{path: 'footer', component: FooterComponent,},
+  {path: 'presentation', component: PresentationComponent,},
+  {path: 'contact', component: ContactComponent,},
+  {path: 'bookmarker', component: BookmarkerComponent,},
+  {path: 'login', component: LoginComponent,}
+];
